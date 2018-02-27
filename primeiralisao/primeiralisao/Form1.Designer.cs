3c172e7dbd3be1462b28c49e12014ccc9e051e9c
Vitor Santos <36664028+dohvakindark@users.noreply.github.com>
2018-02-27 09:12:10 -0300

    Add files via upload
    This is supose to be a simple calculator

---
+++ primeiralisao/primeiralisao/Form1.Designer.cs
@@ -0,0 +1,156 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class frmPrincipal
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnSomar = new System.Windows.Forms.Button();
+            this.btnSubtrair = new System.Windows.Forms.Button();
+            this.btnMult = new System.Windows.Forms.Button();
+            this.btnDividir = new System.Windows.Forms.Button();
+            this.lblValor1 = new System.Windows.Forms.Label();
+            this.lblValor2 = new System.Windows.Forms.Label();
+            this.lblResultado = new System.Windows.Forms.Label();
+            this.txbValor1 = new System.Windows.Forms.TextBox();
+            this.txbValor2 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnSomar
+            // 
+            this.btnSomar.Location = new System.Drawing.Point(344, 72);
+            this.btnSomar.Name = "btnSomar";
+            this.btnSomar.Size = new System.Drawing.Size(75, 23);
+            this.btnSomar.TabIndex = 0;
+            this.btnSomar.Text = "Somar";
+            this.btnSomar.UseVisualStyleBackColor = true;
+            this.btnSomar.Click += new System.EventHandler(this.btnSomar_Click);
+            // 
+            // btnSubtrair
+            // 
+            this.btnSubtrair.Location = new System.Drawing.Point(344, 117);
+            this.btnSubtrair.Name = "btnSubtrair";
+            this.btnSubtrair.Size = new System.Drawing.Size(75, 23);
+            this.btnSubtrair.TabIndex = 1;
+            this.btnSubtrair.Text = "Subtrair";
+            this.btnSubtrair.UseVisualStyleBackColor = true;
+            this.btnSubtrair.Click += new System.EventHandler(this.btnSubtrair_Click);
+            // 
+            // btnMult
+            // 
+            this.btnMult.Location = new System.Drawing.Point(344, 163);
+            this.btnMult.Name = "btnMult";
+            this.btnMult.Size = new System.Drawing.Size(75, 23);
+            this.btnMult.TabIndex = 2;
+            this.btnMult.Text = "Multiplicar";
+            this.btnMult.UseVisualStyleBackColor = true;
+            this.btnMult.Click += new System.EventHandler(this.btnMult_Click);
+            // 
+            // btnDividir
+            // 
+            this.btnDividir.Location = new System.Drawing.Point(344, 212);
+            this.btnDividir.Name = "btnDividir";
+            this.btnDividir.Size = new System.Drawing.Size(75, 23);
+            this.btnDividir.TabIndex = 3;
+            this.btnDividir.Text = "Dividir";
+            this.btnDividir.UseVisualStyleBackColor = true;
+            this.btnDividir.Click += new System.EventHandler(this.btnDividir_Click);
+            // 
+            // lblValor1
+            // 
+            this.lblValor1.AutoSize = true;
+            this.lblValor1.Location = new System.Drawing.Point(86, 63);
+            this.lblValor1.Name = "lblValor1";
+            this.lblValor1.Size = new System.Drawing.Size(40, 13);
+            this.lblValor1.TabIndex = 4;
+            this.lblValor1.Text = "Valor 1";
+            // 
+            // lblValor2
+            // 
+            this.lblValor2.AutoSize = true;
+            this.lblValor2.Location = new System.Drawing.Point(86, 149);
+            this.lblValor2.Name = "lblValor2";
+            this.lblValor2.Size = new System.Drawing.Size(40, 13);
+            this.lblValor2.TabIndex = 5;
+            this.lblValor2.Text = "Valor 2";
+            // 
+            // lblResultado
+            // 
+            this.lblResultado.AutoSize = true;
+            this.lblResultado.Location = new System.Drawing.Point(50, 233);
+            this.lblResultado.Name = "lblResultado";
+            this.lblResultado.Size = new System.Drawing.Size(0, 13);
+            this.lblResultado.TabIndex = 6;
+            // 
+            // txbValor1
+            // 
+            this.txbValor1.Location = new System.Drawing.Point(53, 79);
+            this.txbValor1.Name = "txbValor1";
+            this.txbValor1.Size = new System.Drawing.Size(100, 20);
+            this.txbValor1.TabIndex = 7;
+            // 
+            // txbValor2
+            // 
+            this.txbValor2.Location = new System.Drawing.Point(53, 165);
+            this.txbValor2.Name = "txbValor2";
+            this.txbValor2.Size = new System.Drawing.Size(100, 20);
+            this.txbValor2.TabIndex = 8;
+            // 
+            // frmPrincipal
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(486, 386);
+            this.Controls.Add(this.txbValor2);
+            this.Controls.Add(this.txbValor1);
+            this.Controls.Add(this.lblResultado);
+            this.Controls.Add(this.lblValor2);
+            this.Controls.Add(this.lblValor1);
+            this.Controls.Add(this.btnDividir);
+            this.Controls.Add(this.btnMult);
+            this.Controls.Add(this.btnSubtrair);
+            this.Controls.Add(this.btnSomar);
+            this.Name = "frmPrincipal";
+            this.Text = "Calculadora basica";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnSomar;
+        private System.Windows.Forms.Button btnSubtrair;
+        private System.Windows.Forms.Button btnMult;
+        private System.Windows.Forms.Button btnDividir;
+        private System.Windows.Forms.Label lblValor1;
+        private System.Windows.Forms.Label lblValor2;
+        private System.Windows.Forms.Label lblResultado;
+        private System.Windows.Forms.TextBox txbValor1;
+        private System.Windows.Forms.TextBox txbValor2;
+    }
+}
+
---
+++ primeiralisao/primeiralisao/Form1.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    public partial class frmPrincipal : Form
+    {
+        public frmPrincipal()
+        {
+            InitializeComponent();
+        }
+
+
+
+        private void btnSomar_Click(object sender, EventArgs e)
+        {
+            double val1 = System.Convert.ToDouble(txbValor1.Text);
+            double val2 = System.Convert.ToDouble(txbValor2.Text);
+            double val3 = val1 + val2;
+            lblResultado.Text = "o resultado é: " + val3;
+        }
+
+        private void btnSubtrair_Click(object sender, EventArgs e)
+        {
+            double val1 = System.Convert.ToDouble(txbValor1.Text);
+            double val2 = System.Convert.ToDouble(txbValor2.Text);
+            double val3 = val1 - val2;
+            lblResultado.Text = "o resultado é: " + val3;
+        }
+
+        private void btnMult_Click(object sender, EventArgs e)
+        {
+            double val1 = System.Convert.ToDouble(txbValor1.Text);
+            double val2 = System.Convert.ToDouble(txbValor2.Text);
+            double val3 = val1 * val2;
+            lblResultado.Text = "o resultado é: " + val3;
+
+        }
+
+        private void btnDividir_Click(object sender, EventArgs e)
+        {
+            double val1 = System.Convert.ToDouble(txbValor1.Text);
+            double val2 = System.Convert.ToDouble(txbValor2.Text);
+            if (val2 == 0)
+            {
+                lblResultado.Text = "Impossivel calcular";
+            }
+            else
+            {
+                double val3 = val1 * val2;
+                lblResultado.Text = "o resultado é: " + val3;
+            }
+
+
+        }
+    }
+}
